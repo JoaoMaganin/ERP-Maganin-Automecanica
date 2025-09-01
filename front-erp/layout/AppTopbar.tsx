cd3dad732e8513b8a52f915562e824fe06f3a00f
JoaoMaganin <joaovitormontemor138@gmail.com>
2025-09-01 11:14:15 -0300

    feat: criando botao de backup no front e backup automatico diario

--- front-erp/layout/AppTopbar.tsx
+++ front-erp/layout/AppTopbar.tsx
@@ -5,6 +5,7 @@ import { classNames } from 'primereact/utils';
 import React, { forwardRef, useContext, useImperativeHandle, useRef } from 'react';
 import { AppTopbarRef } from '@/types';
 import { LayoutContext } from './context/layoutcontext';
+import BackupComponent from '@/app/components/Backup/BackupComponent';
 
 const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
     const { layoutConfig, layoutState, onMenuToggle, showProfileSidebar } = useContext(LayoutContext);
@@ -35,6 +36,7 @@ const AppTopbar = forwardRef<AppTopbarRef>((props, ref) => {
             </button>
 
             <div ref={topbarmenuRef} className={classNames('layout-topbar-menu', { 'layout-topbar-menu-mobile-active': layoutState.profileSidebarVisible })}>
+                <BackupComponent />
                 <button type="button" className="p-link layout-topbar-button">
                     <i className="pi pi-calendar"></i>
                     <span>Calendar</span>
